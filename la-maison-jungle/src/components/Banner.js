b27749844aa92eea8b2d706733d993840c1377e2
Marion Vives <mvives.dev@gmail.com>
2025-07-24 17:12:12 +0200

    add style on cart

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,7 +1,13 @@
 import '../styles/Banner.css'
+import logo from '../assets/logo.png'
 
 function Banner() {
-    return <div className='lmj-banner'><h1>La maison jungle</h1></div>
+  const title = 'La maison jungle'
+    return (
+    <div className='lmj-banner'>
+      <img src={logo} alt='Logo La Maison Jungle' className='lmj-logo' />
+      <h1 className='lmj-title'>{title}</h1>
+      </div>)
 }
 
 export default Banner
