1ac6a34ffd721a5d96c8deca60b6b9c67bcd9edd
Marion Vives <mvives.dev@gmail.com>
2025-07-24 18:44:38 +0200

    add sales message when plants has special offer

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -3,7 +3,12 @@ import Cart from './Cart'
 import ShoppingList from './ShoppingList';
 
 function App() {
-  return <div><Banner /> <Cart /></div>;
+  return (
+  <div>
+    <Banner />
+    {<Cart />}
+    <ShoppingList />
+  </div>)
 }
 
 export default App;
