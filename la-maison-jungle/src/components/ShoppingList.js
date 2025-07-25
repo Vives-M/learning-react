c3a7ac217a6fa88d63dd4a4237d30d11bf123d08
Marion Vives <mvives.dev@gmail.com>
2025-07-25 11:19:14 +0200

    create CareScale component

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -1,28 +1,33 @@
-import {plantList} from '../datas/plantList'
+import { plantList } from '../datas/plantList'
+import CareScale from './CareScale'
 import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-  const categories = plantList.reduce(
-    (acc, plant) =>
-      acc.includes(plant.category) ? acc : acc.concat(plant.category),
-    []
-  )
-  return (
-    <div>
-      <ul>
-        {categories.map((cat) => (
-          <li key={cat}>{cat}</li>
-        ))}
-      </ul>
-      <ul className='lmj-plant-list'>
-          {plantList.map((plant) => (
-            <li key={plant.id} className='lmj-plant-item'>{plant.name}
-              {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-            </li>
-          ))}
-      </ul>
-    </div>
-  )
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.isBestSale && <span>🔥</span>}
+						{plant.name}
+						<CareScale careType='water' scaleValue={plant.water} />
+						<CareScale careType='light' scaleValue={plant.light} />
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
