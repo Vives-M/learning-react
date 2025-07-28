657a3cf8ea48ed30c56afa6620d91220e354321f
Marion Vives <mvives.dev@gmail.com>
2025-07-28 14:12:44 +0200

    replace rangeElem by scaleValue in the plantNeeds function

--- la-maison-jungle/src/components/CareScale.js
+++ la-maison-jungle/src/components/CareScale.js
@@ -6,34 +6,34 @@ function CareScale({ scaleValue, careType }) {
 	const scaleType = careType === 'light' ? (<img src={sun} alt='sun' />) : (<img src={water} alt='water' />)
 
 	return (
-		<div>
+		<div >
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
-					<span key={rangeElem.toString()} onClick={() => planteNeeds(careType, rangeElem)}>{scaleType}</span>
+					<span key={rangeElem.toString()} onClick={() => planteNeeds(careType, scaleValue)}>{scaleType}</span>
 				) : null
 			)}
 		</div>
 	)
 }
 
-function planteNeeds(careType, rangeElem){
-  if(careType === 'light' && rangeElem === 1) {
+function planteNeeds(careType, scaleValue){
+  if(careType === 'light' && scaleValue === 1) {
 
     alert('Cette plante nécéssite un peu de lumière')
   }
-  else if(careType === 'light' && rangeElem === 2){
+  else if(careType === 'light' && scaleValue === 2){
     alert('Cette plante nécéssite modérément de la lumière')
   }
-    else if(careType === 'light' && rangeElem === 3){
+    else if(careType === 'light' && scaleValue === 3){
     alert('Cette plante nécéssite beaucoup de lumière')
   }
-  else if (careType === 'water' && rangeElem === 1) {
+  else if (careType === 'water' && scaleValue === 1) {
     alert('Cette plante nécéssite un peu d\'arrosage')
   }
-  else if (careType === 'water' && rangeElem === 2 ) {
+  else if (careType === 'water' && scaleValue === 2 ) {
     alert('Cette plante nécéssite modérément de l\'arrosage')
   }
-    else if (careType === 'water' && rangeElem === 3 ) {
+    else if (careType === 'water' && scaleValue === 3 ) {
     alert('Cette plante nécéssite beaucoup d\'arrosage')
   }
 }
