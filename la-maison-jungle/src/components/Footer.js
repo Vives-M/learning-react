1b3f5f4852b47eafdd497249df727801b8f66ebe
Marion Vives <mvives.dev@gmail.com>
2025-07-28 14:48:59 +0200

    add functions to save user emails and put an alert if there is not @

--- la-maison-jungle/src/components/Footer.js
+++ la-maison-jungle/src/components/Footer.js
@@ -4,14 +4,34 @@ import '../styles/Footer.css'
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
 
+  function handleInput(e) {
+    setInputValue(e.target.value)
+  }
+
+  function handleBlur() {
+      if (!inputValue.includes('@')) {
+        alert('Attention, il n\'y a pas d\'@, ceci n\'est pas une adrese valide !')
+      }
+  }
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+      <input type='email'
+      value={inputValue}
+      placeholder='Votre adresse email ici'
+      onChange={handleInput}
+      onBlur={handleBlur}/>
+
 		</footer>
 	)
+
+
 }
+
+
+
 
 export default Footer
