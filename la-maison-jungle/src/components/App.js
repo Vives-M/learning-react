a1810f3bf2e18f303970b1228239f7b263b2aac6
Marion Vives <mvives.dev@gmail.com>
2025-07-28 13:32:51 +0200

    delete component cart and change title

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,14 +1,18 @@
 import Banner from './Banner'
-import Cart from './Cart'
+// import Cart from './Cart'
+import logo from '../assets/logo.png'
 import ShoppingList from './ShoppingList';
 
 function App() {
   return (
   <div>
-    <Banner />
-    {<Cart />}
+    <Banner>
+      <img src={logo} alt='Logo La maison jungle' className='lmj-logo'/>
+      <h1 className='lmj-title'>La maison jungle</h1>
+    </Banner>
+    {/* {<Cart />} */}
     <ShoppingList />
-    
+
   </div>)
 }
 
